0754bca740dbce49ac6fb4a6bf33de679dc7fb82
Hirohito <h_16110s@yahoo.co.jp>
2018-11-05 00:06:04 +0900

    disp json data

--- app/src/main/java/com/example/crepro/MainActivity.java
+++ app/src/main/java/com/example/crepro/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.crepro;
 
+import android.content.res.Resources;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,13 +12,13 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class MainActivity extends AppCompatActivity {
     private TextToSpeech tts ;
     ListView lv;
-    private static String[] name;
+    private static String jsonData = "";
+    private  String[] name;
 
     private static String[] foods = {
         "Apple", "Banana","Hello World"
@@ -29,34 +30,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tts = new TextToSpeech(this, null);
         lv = (ListView) findViewById(R.id.listView);
+        Resources res = this.getResources();
+        InputStream stream_in = res.openRawResource(R.raw.pokemon_data);
+        String s ="";
         try{
-            FileInputStream fileInputStream = new FileInputStream( getFilesDir()+"pokemon_data.json");
-            byte[] buffer = new byte[fileInputStream.available()];
-            fileInputStream.read(buffer);
-            fileInputStream.close();
+            Reader reader = new InputStreamReader(stream_in);
+            byte[] buffer = new byte[stream_in.available()];
+            while((stream_in.read(buffer)) != -1) {}
+            s = new String(buffer);
 
             // 読み込んだ内容をJSONArrayにパース
-            String json = new String(buffer);
-            JSONArray jsonArray = new JSONArray(json);
-
+            JSONArray jsonArray = new JSONArray(s);
             // パースした内容からListオブジェクトを作成
             for(int i = 0; i < jsonArray.length(); i++) {
                 JSONObject jsonObject = (JSONObject) jsonArray.get(i);
-                name[i] = jsonObject.get("name").toString();
-//                Item item = new Item();
-//                item.setId((int) jsonObject.get("id"));
-//                item.setName((String) jsonObject.get("name"));
-//                items.add(item);
+                JSONObject status = jsonObject.getJSONObject("stats");
+                Log.d("NAME_OUT" ,jsonObject.getString("name") );
+                Log.d("HP_OUT" ,status.getString("hp"));
             }
-        }catch (IOException e){
+        } catch (IOException e){
             Log.e("IO", e.toString());
-        }catch (JSONException e){
+        } catch (JSONException e){
             Log.e("JSON", "json error");
         }
 
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,foods);
         lv.setAdapter(arrayAdapter);
+
+        try {
+            stream_in.close();
+        } catch (IOException e) {
+            Log.e("IO","Close Error");
+        }
     }
 
 
